b9191ef212cbb05a552d1b567be588f1df2de4b5
DaniAGalvez <danieer.galvez@ua.la>
2021-11-01 19:14:56 -0500

    Main layout

--- app/src/main/java/com/danieer/newsapp/MainActivity.kt
+++ app/src/main/java/com/danieer/newsapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.danieer.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.danieer.newsapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        setupBottomNavBar(binding.bottomMainNavBar)
+    }
+
+    private fun setupBottomNavBar(bottomNavigationView: BottomNavigationView) {
+        bottomNavigationView.apply {
+            setOnItemSelectedListener {
+                handleSectionClicked(it)
+                true
+            }
+        }
+    }
+
+    private fun handleSectionClicked(sectionItem: MenuItem) {
+
     }
 }
